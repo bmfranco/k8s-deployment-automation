621425b27c5226a94753155e5ab305ce5622bd8b
bmfranco <bia_moraes2@hotmail.com>
2026-04-13 15:40:28 -0300

    ajustes user_data

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-set -ex
-
-# Log
-exec > /var/log/user-data.log 2>&1
-
-# Atualizar sistema
 yum update -y
 
-# Instalar Docker
-amazon-linux-extras install docker -y
+# Instalar dependências
+yum install -y docker git
+
+# Iniciar Docker
 systemctl enable docker
 systemctl start docker
 
-# Dar permissão ao usuário
+# Adicionar usuário ao grupo docker
 usermod -aG docker ec2-user
 
-# Esperar docker subir
-sleep 20
-
-# Instalar kubectl (binário direto)
+# Instalar kubectl
 curl -o /usr/local/bin/kubectl https://dl.k8s.io/release/v1.29.0/bin/linux/amd64/kubectl
 chmod +x /usr/local/bin/kubectl
 
@@ -30,5 +23,5 @@ chmod +x /usr/local/bin/minikube
 # Instalar Helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-# Rodar Minikube como ec2-user
+# Iniciar Minikube
 su - ec2-user -c "minikube start --driver=docker"
